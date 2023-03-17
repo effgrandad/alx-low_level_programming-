b3d2d2880dc68eb025a22d03e26ed13ce582c8f0
effgrandad <effgrandad@gmail.com>
2023-03-16 17:10:38 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Return 0 (successful)
+ */
+int main(void)
+{
+printf("size of char: %lu bytes(S)", sizeof(char));
+printf("size of int: %lu byte(S)", sizeof(int));
+printf("size of a long int: %lu byte(s)", sizeof(long int));
+printf("size of a long long int: %lu byte(s)", sizeof(long long int));
+printf("size of a float int: %lu byte(s)", sizeof(float));
+return (0);
+}
